7c7719be0e5a8644ac6d3d98076f16c72509cead
Prabhat Sahrawat <97847933+prabhat-021@users.noreply.github.com>
2022-09-14 14:33:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./styles.css";
-
+import Navbar from "./Navbar.js";
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import img1 from "./images/download.webp";
+export default function Navbar() {
+  return (
+    <section className="navbar">
+      <div>
+        <img src={img1} />
+      </div>
+    </section>
+  );
+}
