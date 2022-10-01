799627d0726398803a354aa7f4ee533c4f48395a
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-10-01 21:58:26 +0530

    about and project Header added

--- src/About.js
+++ src/About.js
@@ -1,9 +1,18 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 export default function About(){
     return (
         <section>
     <Navbar />
+    <div className="about-header-1"> 
+    <h1 className="about-h1">About Me</h1>
+    <header className="about-header">
+        <div><Link className="about-link" to="/home">Home </Link></div>
+        <div className="about-link sy1">🠒</div>
+        <div><Link className="about-link" to="/about">About </Link></div>
+    </header>
+    </div>
     <Footer />
     </section>
     );
--- src/Project.js
+++ src/Project.js
@@ -1,9 +1,18 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 export default function Project(){
     return (
         <section>
     <Navbar />
+    <div className="about-header-1"> 
+    <h1 className="about-h1">Projects</h1>
+    <header className="about-header">
+        <div><Link className="about-link" to="/home">Home </Link></div>
+        <div className="about-link sy1">🠒</div>
+        <div><Link className="about-link" to="/project">Project </Link></div>
+    </header>
+    </div>
     <Footer />
     </section>
     );
