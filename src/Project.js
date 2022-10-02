4efe95d6c566eca24307721da52179584223b354
prabhat_021 <prabhatsahrawat010203@gmail.com>
2022-10-02 23:04:52 +0530

    Project section completed

--- src/Project.js
+++ src/Project.js
@@ -1,19 +1,58 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
-export default function Project(){
+import aryan from "./images/aryan.jpeg";
+import ProjectData from "./ProjectData.js";
+
+function ncard(val) {
+    return (
+        <ProjectCard
+            src={val.imgsrc}
+            tittle={val.title}
+            details={val.details}
+            link={val.link}
+        />
+    );
+}
+
+function ProjectCard(props) {
+    return (
+        <div className="project-card">
+            <div>
+                <img className="project-img" src={props.src} alt="picture" />
+            </div>
+            <div className="project-title">
+                {props.tittle}
+            </div>
+            <div className="project-details">{props.details}</div>
+            <div><a href={props.link} className="project-link" target="_blank">GitHub Link</a></div>
+        </div>
+    );
+}
+
+
+export default function Project() {
+
+
     return (
         <section>
-    <Navbar />
-    <div className="about-header-1"> 
-    <h1 className="about-h1">Projects</h1>
-    <header className="about-header">
-        <div><Link className="about-link" to="/home">Home </Link></div>
-        <div className="about-link sy1">🠒</div>
-        <div><Link className="about-link" to="/project">Project </Link></div>
-    </header>
-    </div>
-    <Footer />
-    </section>
+            <Navbar />
+            <header className="about-header-1">
+                <h1 className="about-h1">Projects</h1>
+                <div className="about-header">
+                    <div><Link className="about-link" to="/home">Home </Link></div>
+                    <div className="about-link sy1">🠒</div>
+                    <div><Link className="about-link" to="/project">Project </Link></div>
+                </div>
+            </header>
+            <section className="project-main">
+                <h1 className="head-h2">My Latest Featured Projects</h1>
+                <p className="a-para">You can also go to My <a href="https://github.com/prabhat-021" className="a-link">GitHub</a> to check my Progress</p>
+                <section className="project-card-1">
+                    {ProjectData.map(ncard)}
+                </section>
+            </section>
+            <Footer />
+        </section>
     );
 }
---
+++ src/ProjectData.js
@@ -0,0 +1,86 @@
+import img1 from "./images/converter.png";
+import img2 from "./images/meme.png";
+import img3 from "./images/tindog.png";
+import img4 from "./images/keeper.png";
+import img5 from "./images/todolist.png";
+import img6 from "./images/tedxkiet.png";
+import img7 from "./images/drumkit.png";
+import img8 from "./images/dicegame.png";
+import img9 from "./images/regform.jpg";
+import img10 from "./images/CV.png";
+
+
+
+const ProjectData = [
+    {
+        id: 1,
+        imgsrc: img1,
+        title: "CurrencyRates",
+        details: "HTML,CSS,JAVASCRIPT,REACT",
+        link: "https://github.com/prabhat-021/CurrencyRates"
+    },
+    {
+        id: 2,
+        imgsrc: img2,
+        title: "MemeGenerator",
+        details: "HTML,CSS,JAVASCRIPT,REACT",
+        link: "https://github.com/prabhat-021/MemeGenerator"
+    },
+    {
+        id: 3,
+        imgsrc: img3,
+        title: "TinDog ",
+        details: "HTML,CSS,BOOTSTRAP,JAVASCRIPT",
+        link: "https://prabhat-021.github.io/TinDog/"
+    },
+    {
+        id: 4,
+        imgsrc: img4,
+        title: "Keeper App",
+        details: "HTML,CSS,JAVASCRIPT,REACT",
+        link: "https://prabhat-021.github.io/KeeperApp/"
+    },
+    {
+        id: 5,
+        imgsrc: img5,
+        title: "Todo-List",
+        details: "HTML,CSS,JAVASCRIPT,REACT",
+        link: "https://prabhat-021.github.io/todolist/"
+    }, 
+    {
+        id: 6,
+        imgsrc: img6,
+        title: "TEDxKIET Clone",
+        details: "HTML,CSS,BOOTSTRAP,JAVASCRIPT",
+        link: "https://prabhat-021.github.io/tedxkiet/"
+    },
+    {
+        id: 7,
+        imgsrc: img7,
+        title: "Drum Kit",
+        details: "HTML,CSS,JAVASCRIPT",
+        link: "https://prabhat-021.github.io/drum-kit/"
+    },
+    {
+        id: 8,
+        imgsrc: img8,
+        title: "Dice Game",
+        details: "HTML,CSS,JAVASCRIPT",
+        link: "https://prabhat-021.github.io/dice-game/"
+    },
+    {
+        id: 9,
+        imgsrc: img9,
+        title: "Registration from",
+        details: "HTML, CSS , JAVASCRIPT",
+        link: "https://prabhat-021.github.io/registration-form/"
+    },
+    {
+        id: 10,
+        imgsrc: img10,
+        title: "CV(My first HTML page)",
+        details: "HTML",
+        link: "https://prabhat-021.github.io/cv/"
+    }
+]
+export default ProjectData;
